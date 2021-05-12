32319e40068e3534d0233bc566d7cd158f18e693
Miguel Angel <miguelksangel@gmail.com>
2021-05-12 02:45:52 -0600

    API

--- API/server/src/rutas.js
+++ API/server/src/rutas.js
@@ -1,12 +1,14 @@
 /*IMPORTS*/
-const Router  = require("express");
+const Router = require("express");
 const Vacunacion = require("./schemas/Vacunacion");
 const fs = require('fs');
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
 
 /*VARIABLES A UTILIZAR*/
 const app = Router();
 let top = new Array;
 let gen = new Array;
+let ult = new Array;
 
 
 /* Datos almacenados en la base de datos, en MongoDB */
@@ -14,165 +16,85 @@ let gen = new Array;
 /* Los últimos cinco vacunados almacenados por país, en MongoDB */
 
 
+app.get("/", async (req, res) => {
+    console.log("GET")
+    limpieza()
+    Vacunacion.find()
+        .then(result => {
+            console.log(result)
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err)
+            res.send(err);
+        });
+});
+
 /*SERVICIOS FINALES*/
 /* Datos almacenados en la base de datos, en MongoDB */
 app.post("/find", async (req, res) => {
+    console.log("FIND")
+    limpieza()
     Vacunacion.find()
-     .then(result => {
-        res.send(result);
-     })
-     .catch(err => {
-         console.log(err)
-         res.send(err);
-     });
+        .then(result => {
+            console.log(result)
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err)
+            res.send(err);
+        });
 });
 
 /* Gráfica de pie de los géneros de los vacunados por país, en MongoDB */
 app.post("/genero", async (req, res) => {
+    console.log("GENERO")
+    limpieza()
     Vacunacion.find()
-     .then(result => {
-        res.send(genero(result));
-     })
-     .catch(err => {
-         console.log(err)
-         res.send(err);
-     });
+        .then(result => {
+            res.send(genero(result));
+        })
+        .catch(err => {
+            console.log(err)
+            res.send(err);
+        });
 });
 
-/*REGIÓN MÁS INFECTADA*/
-app.post("/region", async (req, res) => {
-    Vacunacion.find()
-     .then(result => {
-        res.send(region(result));
-     })
-     .catch(err => {
-         console.log(err)
-         res.send(err);
-     });
-});
-
-/*TOP 5 DEPARTAMENTOS INFECTADOS*/
-app.post("/funnel", async (req, res) => {
-    Vacunacion.find()
-     .then(result => {
-        res.send(top5Deptos(result));
-     })
-     .catch(err => {
-         console.log(err)
-         res.send(err);
-     });
-});
-
-/*PORCENTAJE DE Vacunacion INFECTADOS POR STATE*/
-app.post("/circular1", async (req, res) => {
-    Vacunacion.find()
-     .then(result => {
-        res.send(infectados(result, "state"));
-     })
-     .catch(err => {
-         console.log(err)
-         res.send(err);
-     });
-});
-
-/*PORCENTAJE DE Vacunacion INFECTADOS POR INFECTEDTYPE*/
-app.post("/circular2", async (req, res) => {
-    Vacunacion.find()
-     .then(result => {
-        res.send(infectados(result, "infectedtype"));
-     })
-     .catch(err => {
-         console.log(err)
-         res.send(err);
-     });
-});
-
-/*ÚLTIMOS 5 Vacunacion REGISTRADOS*/
+/* Los últimos cinco vacunados almacenados por país, en MongoDB */
 app.post("/ultimos", async (req, res) => {
+    console.log("ULTIMOS")
+    limpieza()
     Vacunacion.find()
-     .then(result => {
-        res.send(ultimos(result));
-     })
-     .catch(err => {
-         console.log(err)
-         res.send(err);
-     });
-});
-
-/*RANGO DE EDAD DE INFECTADOS*/
-app.post("/edades", async (req, res) => {
-    Vacunacion.find()
-     .then(result => {
-        res.send(edades(result));
-     })
-     .catch(err => {
-         console.log(err);
-         res.send(err);
-     });
-});
-
-/*LECTURA DE MEMORIA RAM*/
-app.post("/ram", async (req, res) => {
-    fs.readFile("./lecturas/ram_proyecto1", 'utf-8', (err, data) => {
-        //console.log(data);
-        let d = JSON.parse(data);
-        console.log(d);
-        if(err) {
+        .then(result => {
+            res.send(ultimos(result));
+        })
+        .catch(err => {
             console.log(err)
             res.send(err);
-        } else {
-            res.send(d)
-        }
-    });
+        });
 });
 
-/*LECTURA DE MEMORIA CPU*/
-app.post("/cpu", async (req, res) => {
-    fs.readFile("./lecturas/cpu_proyecto1", 'utf-8', (err, data) => {
-        //console.log(data);
-        let d = JSON.parse(data);
-        console.log(d);
-        if(err) {
-            console.log(err)
-            res.send(err);
-        } else {
-            res.send(d)
-        }
-    });
-});
-
-/*LECTURA DE MEMORIA RAM*/
-app.post("/clean", async (req, res) => {
-    Vacunacion.remove()
-     .then(result => {
-        res.send(result);
-     })
-     .catch(err => {
-         console.log(err);
-         res.send(err);
-     });
-});
 
 
 /*METODOS VARIOS*/
-function genero(result){
+function genero(result) {
     gen = new Array;
-    
-    for(let i of result){
+
+    for (let i of result) {
         agregarPais(i)
     }
 
-    //console.log(gen)
+    console.log(gen)
     return gen
 }
 
-function agregarPais(dato){
+function agregarPais(dato) {
     let exist = false;
-    for(let i in gen){
-        if(gen[i][0] == dato.location){
-            if(dato.gender == 'male'){
+    for (let i in gen) {
+        if (gen[i][0] == dato.location) {
+            if (dato.gender == 'male') {
                 gen[i][1]++;
-            }else{
+            } else {
                 gen[i][2]++;
             }
             exist = true;
@@ -180,183 +102,73 @@ function agregarPais(dato){
         }
     }
 
-    if(!exist){
-        if(dato.gender == 'male'){
+    if (!exist) {
+        if (dato.gender == 'male') {
             gen.push([dato.location, 1, 0]);
-        }else{
+        } else {
             gen.push([dato.location, 0, 1]);
         }
     }
 }
 
+function ultimos(result) {
+    ult = new Array;
 
-
-
-
-function getRegion(location){
-    let loc = location.toLowerCase();
-    if(loc == "quetzaltenango" || loc == "retalhuleu" || loc == "san marcos" || loc == "suchitepéquez" || loc == "sololá" || loc == "totonicapán"){
-        return "Suroccidente"
-    }else if(loc == "guatemala"){
-        return "Metropolitana"
-    }else if(loc == "huehuetenango" || loc == "quiché"){
-        return "Noroccidente"
-    }else if(loc == "chimaltenango" || loc == "sacatepéquez" || loc == "escuintla"){
-        return "Central"
-    }else if(loc == "alta verapaz" || loc == "baja verapaz"){
-        return "Verapaz"
-    }else if(loc == "chiquimula" || loc == "el progreso" || loc == "izabal" || loc == "zacapa"){
-        return "Nororiente"
-    }else if(loc == "jutiapa" || loc == "jalapa" || loc == "santa rosa"){
-        return "Suroriente"
-    }else if(loc == "petén"){
-        return "Petén"
-    }else{
-        return location
-    }
-}
-
-function region(result){
-    top = new Array;
-    
-    for(let i of result){
-        agregarTop(i)
+    for (let i of result) {
+        agregarDato(i)
     }
 
-    for(var i=1;i<top.length;i++){
-        for(var j=0; j<(top.length-i); j++){
-            if(top[j][0] < top[j+1][0]){
-                k = top[j+1][0];
-                valor = top[j+1][1];
-                
-                top[j+1][0] = top[j][0];
-                top[j+1][1] = top[j][1];
-                
-                top[j][0] = k;
-                top[j][1] = valor;
-            }
-        }
-    }
+    //console.log(ult)
 
-    let aux = new Array;
-    for(var i=0; i < top.length; i++){
-        if(i < 1){
-            aux.push(top[i])
-            break;
-        }
-    }
+    getUltimos();
 
     //console.log(top)
-    return aux
-}
-
-function top5Deptos(result){
-    top = new Array;
-    
-    for(let i of result){
-        agregarTop(i.location)
-    }
-
-    for(var i=1;i<top.length;i++){
-        for(var j=0; j<(top.length-i); j++){
-            if(top[j][0] < top[j+1][0]){
-                k = top[j+1][0];
-                valor = top[j+1][1];
-                
-                top[j+1][0] = top[j][0];
-                top[j+1][1] = top[j][1];
-                
-                top[j][0] = k;
-                top[j][1] = valor;
-            }
-        }
-    }
-
-    let aux = new Array;
-    for(var i=0; i < top.length; i++){
-        if(i < 5){
-            aux.push(top[i])
-        }
-    }
-    //top.prototype.sort()
-
-    console.log(aux)
-    return aux
-}
-
-function infectados(result, tipo){
-    top = new Array;
-    
-    for(let i of result){
-        if(tipo == "state"){
-            agregarTop(i.state)
-        }else if(tipo == "infectedtype"){
-            agregarTop(i.infectedtype)
-        }
-    }
-
-    for(var i=0; i < top.length; i++){
-        top[i][0] = (top[i][0]/result.length)*100
-    }
-
     return top
 }
 
-function agregarTop(dato){
+function agregarDato(dato) {
     let exist = false;
-    for(let i in top){
-        if(top[i][1] == dato){
-            top[i][0]++;
+    for (let i in ult) {
+        if (ult[i][0] == dato.location) {
+            ult[i][1].push(dato)
             exist = true;
             break;
         }
     }
 
-    if(!exist){
-        top.push([1, dato]);
+    if (!exist) {
+        ult.push([dato.location, [dato]]);
     }
+
 }
 
-
-
-function ultimos(result){
-    let aux = new Array;
-    let j = 0;
-    for(let i=result.length-1; i >= 0; i--){
-        if(j < 5){
-            j++;
-            aux.push(result[i]);
+function getUltimos() {
+    for (let i of ult) {
+        let v = 5;
+        let j = [];
+        if(i[1] instanceof Array){
+            j = i[1];
         }
-    }
-    return aux;
-}
-
-function edades(result){
-    let lista = [[0,10],[0,20],[0,30],[0,40],[0,50],[0,60],[0,70],[0,80],[0,90]]
-
-    for(let i of result){
-        if(i.age < 10){
-            lista[0][0]++;
-        }else if(i.age < 20){
-            lista[1][0]++;
-        }else if(i.age < 30){
-            lista[2][0]++;
-        }else if(i.age < 40){
-            lista[3][0]++;
-        }else if(i.age < 50){
-            lista[4][0]++;
-        }else if(i.age < 60){
-            lista[5][0]++;
-        }else if(i.age < 70){
-            lista[6][0]++;
-        }else if(i.age < 80){
-            lista[7][0]++;
-        }else{
-            lista[8][0]++;
+        //console.log(j.length)
+        for (let a = j.length; a > 0; a--) {
+            if (v > 0) {
+                //console.log(i[0])
+                //console.log(i[1])
+                //console.log(j[j.length-1])
+                top.push(j.pop())
+                v--;
+            }else{
+                break;
+            }
         }
     }
 
-    return lista;
+}
+
+function limpieza() {
+    top = new Array;
+    gen = new Array;
+    ult = new Array;
 }
 
 module.exports = app;
